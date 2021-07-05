793af01f028bee4049966ba17385970838f00e5c
Ivan Lytvynenko <ivan.lytvynenko.dev@gmail.com>
2021-07-05 17:08:02 +0300

    Add middleware to redux

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import { createStore } from 'redux'
+import { applyMiddleware, createStore } from 'redux'
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
+import { increment, decrement, asyncIncrement } from './redux/actions'
 import { rootReducer } from './redux/rootReducer'
 import './styles.css'
 
@@ -9,21 +12,18 @@ const subBtn = document.getElementById('sub')
 const asyncBtn = document.getElementById('async')
 const themeBtn = document.getElementById('theme')
 
-let store = createStore(rootReducer, 0)
-
+let store = createStore(rootReducer, 0, applyMiddleware(thunk, logger))
 
 addBtn.addEventListener('click', () => {
-  store.dispatch({ type: 'INCREMENT' })
+  store.dispatch(increment())
 })
 
 subBtn.addEventListener('click', () => {
-  store.dispatch({ type: 'DECREMENT' })
+  store.dispatch(decrement())
 })
 
 asyncBtn.addEventListener('click', () => {
-  setTimeout(() => {
-    
-  }, 2000)
+  store.dispatch(asyncIncrement())
 })
 
 store.subscribe(() => {
