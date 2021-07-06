6ff05092ab50f598abbaca2e2d6b4134db919b5f
Ivan Lytvynenko <ivan.lytvynenko.dev@gmail.com>
2021-07-06 10:55:03 +0300

    Fix change theme functionality

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { applyMiddleware, createStore } from 'redux'
 import thunk from 'redux-thunk'
 import logger from 'redux-logger'
-import { increment, decrement, asyncIncrement } from './redux/actions'
+import { increment, decrement, asyncIncrement, changeTheme } from './redux/actions'
 import { rootReducer } from './redux/rootReducer'
 import './styles.css'
 
@@ -12,7 +12,7 @@ const subBtn = document.getElementById('sub')
 const asyncBtn = document.getElementById('async')
 const themeBtn = document.getElementById('theme')
 
-let store = createStore(rootReducer, 0, applyMiddleware(thunk, logger))
+let store = createStore(rootReducer, applyMiddleware(thunk, logger))
 
 addBtn.addEventListener('click', () => {
   store.dispatch(increment())
@@ -29,12 +29,15 @@ asyncBtn.addEventListener('click', () => {
 store.subscribe(() => {
   const state = store.getState()
   
-  counter.textContent = state
+  counter.textContent = state.counter
+  document.body.className = state.theme.value 
 })
 
 store.dispatch({ type: 'INIT_APPLICATION' })
 
 themeBtn.addEventListener('click', () => {
-  document.body.classList.toggle('dark')
+  const newTheme = document.body.classList.contains('light') ? 'dark' : 'light'
+
+  store.dispatch(changeTheme(newTheme))
 })
 
